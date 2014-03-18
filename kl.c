0a08759badef36220193143ac3f7fc56bc2c289a
Fanchy <li3939108@gmail.com>
2014-03-18 14:53:35 -0500

    kl.c

---
+++ kl.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char ** argv){
+	if(argc == 2){
+		FILE* input; //input file
+		char *line = NULL;
+		size_t len = 0;
+		ssize_t read;
+
+		input = fopen(argv[1], "r");
+        if (input == NULL){
+			exit(EXIT_FAILURE);}
+
+		while ((read = getline(&line, &len, input)) != -1) {
+        	int num1, num2 ;
+        	char *saveptr ;
+			num1 = atoi( strtok_r(line, " ", &saveptr) );
+			num2 = atoi( strtok_r(NULL, " ", &saveptr) );
+			printf("two numbers read: %d %d\n", num1, num2);
+        }
+        free(line);
+	}
+}
